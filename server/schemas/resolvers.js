215dae6632a583a7d4b9a27d9a36b72b5ff2b50d
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2023-11-02 11:54:43 -0400

    still going

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,66 +1,86 @@
-const { User, Book } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password')
-        
-                return userData;
-            };
-        },
+  // retrieves a specific user by their username. Includes the user's saved books when returning the data
+  Query: {
+    getMe: async (parent, args, context) => {
+      console.log("Running query getMe - retrieving data from signed in user");
+      console.log(context.user);
+      if (context.user) {
+        console.log("Within IF statement in resolvers");
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        console.log("Here is the user data being returned:");
+        console.log(userData);
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }, context) => {
+      // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      // If email and password are correct, sign user into the application with a JWT
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }, context) => {
+      // First we create the user
+      const user = await User.create({ username, email, password });
+      // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
     },
 
-    Mutation: {
-        createUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
+    saveBook: async (parent, args, context) => {
+      console.log("context.user:", context.user);
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args } },
+          { new: true }
+        );
+        console.log(updatedUser);
+        return updatedUser;
+      }
 
-            return { user, token };
-        },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-
-            if (!user) {
-                throw new AuthenticationError(
-                    'Email or Password is incorrect. Please try again.'
-                );
-            }
-            
-            const correctPw = await user.isCorrectPassword(password);
-
-            if(!correctPw) {
-                throw new AuthenticationError(
-                    'Email or Password is incorrect. Please try again.'
-                );
-            }
-
-            const token = signToken(user);
-            return { user, token };
-        },
-        saveBook: async (parent, { saveBook }, context) => {
-            if (context.user) {
-                const userData = await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $push: { savedBooks: saveBook } },
-                    { new: true }
-                );
-
-                return userData;
-            }
-        },
-        deleteBook: async (parent, { bookId }, context) => {
-            if (context.user) {
-                const userData = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId } } },
-                    { new: true }
-                );
-                return userData;
-            }
-        },
+      throw AuthenticationError;
     },
+
+    removeBook: async (parent, { bookId }) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,51 +1,46 @@
-const { gql } = require('apollo-server-express')
+// const { gql } = require("@apollo/server");
 
-const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        password: String!
-        savedBooks: [Book]
-    }
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+    bookCount: Int
+  }
 
-    type Book {
-        _id: ID
-        authors: [String]
-        description: String!
-        bookId: String!
-        image: String
-        link: String
-        title: String!
-    }
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    input bookData {
-        authors: [String]
-        description: String
-        bookId: String!
-        image: String
-        link: String
-        title: String!
-    }
+  type Query {
+    getMe: User
+  }
 
-    type Query {
-        me: User
-    }
-
-    type Mutation {
-        createUser(username: String!, email: String!, password: String!): Auth
-        
-        login(email: String!, password: String!): Auth
-
-        saveBook(savedBook: bookData!): User
-
-        deleteBook(bookId: ID!): User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      authors: [String]
+      description: String
+      bookId: String!
+      image: String
+      link: String
+      title: String!
+    ): User
+    removeBook(bookId: String!): User
+  }
 `;
 
 module.exports = typeDefs;
